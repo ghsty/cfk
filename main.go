3200c32b574d87fc55f7a43f320a2264c6210be9
ghsty <ghostyjungle@hotmail.com>
2022-05-05 23:26:37 +0200

    it didnt work, i fixed it

--- main.go
+++ main.go
@@ -7,29 +7,35 @@ import (
 func main() {
 	fmt.Println("Enter the temperature unit (c/f/k): ")
 	var flag string
-	fmt.Scanf("%s", &flag)
-
-	fmt.Println("Enter the temperature: ")
-	var temp float64
-	fmt.Scanf("%s", &temp)
+	fmt.Scan(&flag)
 
 	if flag == "" {
 		fmt.Println("Please provide a temperature unit (c/f/k).")
 		return
 
 	} else if flag == "c" {
+		fmt.Println("Enter the temperature: ")
+		var temp float64
+		fmt.Scan(&temp)
 		var tempf float64 = temp*1.8 + 32
 		var tempk float64 = temp + 273.15
+		fmt.Println("The temperature you entered is: ", temp)
 		fmt.Println("The temperature in Fahrenheit is: ", tempf)
 		fmt.Println("The temperature in Kelvin is: ", tempk)
 
 	} else if flag == "f" {
-		var tempc float64 = (temp - 32) * 5 / 9
-		var tempk float64 = (temp + 459.67) * 5 / 9
+		fmt.Println("Enter the temperature: ")
+		var temp float64
+		fmt.Scan(&temp)
+		var tempc float64 = (temp - 32) * 0.5556
+		var tempk float64 = (temp + 459.67) / 1.8
 		fmt.Println("The temperature in Celsius is: ", tempc)
 		fmt.Println("The temperature in Kelvin is: ", tempk)
 
 	} else if flag == "k" {
+		fmt.Println("Enter the temperature: ")
+		var temp float64
+		fmt.Scan(&temp)
 		var tempc float64 = temp - 273.15
 		var tempf float64 = 1.8*(temp-273.15) + 32
 		fmt.Println("The temperature in Celsius is: ", tempc)
